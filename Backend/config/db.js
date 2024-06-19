3362c713a2d83c3554db915e19707c601a06832d
maheshwararaj07 <maheshwararaj2003@gmail.com>
2024-06-19 15:39:37 +0530

    Backend init commit

---
+++ Backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()  => {  
+    await mongoose.connect("mongodb+srv://maheshwararaj2003:mahesh0407@cluster0.rwqcpw4.mongodb.net/food-del").then(()=>console.log("DB COnnected"))
+    .catch(()=> console.log("Error occured"));
+}
+
+
+export default connectDB;
